16a2f032ba43c37e0cb0eef3b8c57e0b6ced5c24
Ibrahim <ibrahimzah33r@gmail.com>
2026-08-17 16:25:30 +0100

    Initial Spring Boot project

---
+++ src/test/java/sentinel_backend/SentinelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sentinel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
