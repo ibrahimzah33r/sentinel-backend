199bbf9bba187aee2044ad3f159386c791237881
Ibrahim <ibrahimzah33r@gmail.com>
2026-09-04 15:31:55 +0100

    fix: stabilize paginated API responses

--- src/main/java/sentinel_backend/SentinelBackendApplication.java
+++ src/main/java/sentinel_backend/SentinelBackendApplication.java
@@ -3,12 +3,15 @@ package sentinel_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class SentinelBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SentinelBackendApplication.class, args);
+		SpringApplication.run(
+				SentinelBackendApplication.class,
+				args);
 	}
-
 }
